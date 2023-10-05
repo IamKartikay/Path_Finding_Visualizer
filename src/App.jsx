b663a41de3f1412882b9f242f6c22bae9d53cbbe
IamKartikay <kartikaysingh.business@gmail.com>
2023-10-05 23:13:49 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavComp from "./Components/NavComp";
+import PathfindingVisualizer from "./MainStuff/PathfindingVisualizer";
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavComp />
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/NavComp.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class NavComp extends Component {
+  render() {
+    return (
+      <div>
+        <div className="navbar navbar-expand-lg navbar-dark bg-dark ">
+          <a className="navbar-brand" href="/">
+            <b>By Kartikay Singh & Kirtan Solanki</b>
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
